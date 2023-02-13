6a06adfb97de9b2f36e2933dcfae7879fd5e18d4
SejiMe <atienza.seji@gmail.com>
2023-02-13 20:24:43 +0800

    chore: use classnames and initial setup todo

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -2,18 +2,47 @@ import React from 'react';
 import MainApp from '../../components/layouts/MainApp';
 import { type NextPage } from 'next';
 import CardTodo from '../../components/card/CardTodo';
-
-const index: NextPage  = () => {
-	const date = new Date()
-	const month = date.getMonth() + 1
-	const day = date.getDate()
-	const year = date.getFullYear()
+import classNames from 'classnames';
+import Button from '../../components/custom/Input/Button';
+import { PlusIcon } from '@heroicons/react/24/outline';
+const index: NextPage = () => {
+	const date = new Date();
+	const month = date.getMonth() + 1;
+	const day = date.getDate();
+	const year = date.getFullYear();
 	console.log(day);
 	return (
 		<MainApp>
-			<h1>What&apos;s Up?</h1>
-			<CardTodo title='Work Today!' date={`${month}/${day}/${year}`} />
-			<h2>hello</h2>
+			<div className='relative flex h-screen flex-col content-center items-center gap-3 text-beige-500'>
+				<div
+					className={classNames(
+						'mt-10 flex items-center justify-center gap-4'
+					)}
+				>
+					<h1 className='text-beige-600'>What&apos;s Up?</h1>
+					<h4>Get active today!</h4>
+				</div>
+
+				<div>
+					<Button type='button' classname='text-beige-500 text-xl'>
+						Add Task
+						<PlusIcon className='h-6 w-6 ml-1' />
+					</Button>
+				</div>
+
+				<div className='mt-10 flex flex-col'>
+					<ul>
+						<li>
+							<CardTodo
+								title='Work Today!'
+								date={`${month}/${day}/${year}`}
+								numberOfTask={10}
+								slug='1233'
+							/>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</MainApp>
 	);
 };
