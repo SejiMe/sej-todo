6a67d6376c51197eb26de834fc864d4300ed1209
SejiMe <atienza.seji@gmail.com>
2023-02-13 20:22:35 +0800

    chore: Add Custom Button

--- src/components/custom/Input/Button.tsx
+++ src/components/custom/Input/Button.tsx
@@ -1,13 +1,15 @@
+import classNames from 'classnames';
 import React from 'react';
 
 type Props = {
 	type: 'button' | 'submit';
 	children?: React.ReactNode;
-	onClick: MouseEvent;
+	onClick?: MouseEvent;
+	classname?: string
 };
 
 const Button = (props: Props) => {
-	return <button type={props.type}>{props.children}</button>;
+	return <button type={props.type} className={classNames('border-2 p-2 rounded-md flex font-mono', props.classname)}>{props.children}</button>;
 };
 
 export default Button;
