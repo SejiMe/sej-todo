5208e368d094b3992ae3b99bbb03ca16628bd13f
SejiMe <atienza.seji@gmail.com>
2023-02-13 20:22:54 +0800

    chore: fix mainapp layout

--- src/components/layouts/MainApp.tsx
+++ src/components/layouts/MainApp.tsx
@@ -13,10 +13,10 @@ function MainApp(props: Props): JSX.Element {
 			{/* bg-gradient-to-r from-blue-800 to-indigo-900 */}
 			{/* bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] from-neutral-700 via-teal-600 to-purple-700 */}
 			{/* bg-gradient-to-b from-sky-300 via-emerald-300 to-blue-300  */}
-			<main className='flex min-h-screen flex-col 
-			bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] from-blue-500 via-blue-400 to-blue-500 
-			py-5
-			'>
+			<main
+				className='flex min-h-screen flex-col 
+			bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] from-blue-500 via-blue-400 to-blue-500'
+			>
 				{props.children}
 			</main>
 			<Footer />
